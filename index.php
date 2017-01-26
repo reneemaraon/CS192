2c9d80e89352c076e2d1dbdf2891301ec337aea8
reneeehh <reneemaraon@gmail.com>
2017-01-26 11:33:17 +0800

    First Commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+  <head>
+      <title>AGILITEAM</title>
+
+  </head>
+  <body>
+    <h1 class="ui header">Welcome to UPCAT DATABASE</h1>
+    <a href="login.php"> Click here to login</a>
+    <br/>
+    <a href="register.php"> Click here to register </a>
+  </body>
+</html>
