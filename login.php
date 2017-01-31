fd2dc63575066958ea9a1d277cbc1ff9eb0cbe99
reneeehh <reneemaraon@gmail.com>
2017-02-01 02:05:08 +0800

    Added new stuff

---
+++ login.php
@@ -0,0 +1,27 @@
+<html>
+<link rel="stylesheet" href="styles.css">
+<body>
+
+  <div class="imgcontainer">
+    <img src="pics/logo.png" height="100" width="100" align ="center">
+  </div>
+  <div class="logotext" align = "center">
+        <h2> University of the Philippines Office of Admissions</h2>
+    </div>
+    <div align="center">
+    <form action="action_page.php">
+  <div class="login_container">
+    <label><b>Username</b></label>
+    <input type="text" placeholder="Enter Username" name="uname" required>
+
+    <label><b>Password</b></label>
+    <input type="password" placeholder="Enter Password" name="psw" required>
+        
+    <button type="submit">Login</button>
+  
+  </div>
+  </div>
+</form>
+
+</body>
+</html>
