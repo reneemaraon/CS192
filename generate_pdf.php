1b2ca9394573ed8f2edf3ed2604f081c5aae0cd0
LeeSicat <leesicat1@gmail.com>
2017-02-20 07:48:27 +0800

    Add files via upload
    PDF Generation, Saving responses

---
+++ generate_pdf.php
@@ -0,0 +1,57 @@
+
+								<?php
+								$servername = "localhost";  
+								$username = "root";
+								$password = "";
+								$dbname = "upcatdb";
+
+								// Create connection
+								$conn = new mysqli($servername, $username, $password, $dbname);
+								// Check connection
+								if ($conn->connect_error) {
+										die("Connection failed: " . $conn->connect_error);
+								} 
+
+								$sql = "SELECT * FROM `DIRINFO`  LIMIT 10, 20" ;
+								$result = $conn->query($sql);
+
+								$myfile = fopen("abc.txt", "w") or die("Unable to open file!");
+
+								$txt = "Jane Do2e\n";
+					
+
+								if ($result->num_rows > 0) {
+										// output data of each row
+
+
+										while($row = $result->fetch_assoc()) {
+										
+										fwrite($myfile, $row["ID"]);
+										fwrite($myfile, ";");
+										fwrite($myfile, $row["STATUS"]);
+										fwrite($myfile, ";");
+										fwrite($myfile, $row["NAME"]);
+										fwrite($myfile, ";");
+										fwrite($myfile, $row["YEAR"]);
+										fwrite($myfile, ";");
+										fwrite($myfile, $row["TCTRCODE"]);
+										fwrite($myfile, ";");
+										fwrite($myfile, $row["testcenter"]);
+										fwrite($myfile, ";");
+										fwrite($myfile, $row["ASSG"]);
+										fwrite($myfile, "\n");
+
+								}
+								}
+
+								echo('generated');
+
+
+
+								$conn->close();
+								fclose($myfile);
+
+								
+								?>
+								<!--Filter-->
+
---
+++ invitation.php
@@ -0,0 +1,221 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Home</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<script src="js/bootstrap.min.js"></script>
+</head>
+<script type='text/javascript' src='//code.jquery.com/jquery-1.10.1.js'></script>
+<body>
+  	  <?php
+    session_start();
+    if($_SESSION['user']){
+    }
+    else{
+      header('location: login.php');
+    }
+    $user = $_SESSION['user'];
+  ?>
+<nav class="navbar navbar-default">
+	<div class="container-fluid">
+		<div class="navbar-header">
+		</div>
+		<a href="#" class="navbar-left"><img src="pics/logo.png" width="40px" hspace="20px" vspace="5px"></a>
+		<a class="navbar-brand" href="#">UP Office of Admissions</a>
+		<ul class="nav navbar-nav">
+			<li class="active"><a href="#">Home</a></li>
+			<li><a href="#">Profile</a></li>
+			<li><a href="#">Records</a></li>
+			<li><a href="logout.php">Logout</a></li>
+		</ul>
+	</div>
+</nav>
+
+<!-- baba -->
+	<div class="container-fluid">
+		<div class="row" >
+			<div class="col-sm-2">
+					<ul id="sidebar" class="nav nav-stacked nav-pills" style="color: #660000">
+						<li><a href="home.php">View List of Personnel</a></li>
+						<li><a href="addrecord.php">Add Record</a></li>
+						<li><a href="invitation.php" class="active">Invitation</a></li>
+						<li><a href="response.php">Response</a></li>
+						<li><a href="assignment.php">Assignment</a></li>
+						<li><a href="attendance.php">UPCAT Attendance</a></li>
+
+				</ul>
+			</div>
+			<div class="col-sm-9">
+						<!-- CONTENT -->
+						<h3> Invitation </h3>
+						<button id="button1" type="button">Write to File</button>
+						<br>
+						<form class="form-inline">
+							<label class="sr-only" for="inlineFormInput">Year</label>
+							<input type="text" class="form-control mb-2 mr-sm-2 mb-sm-0" id="inlineFormInput" placeholder="Year">
+
+							<label class="sr-only" for="inlineFormInputGroup">Name</label>
+							<input type="text" class="form-control" id="inlineFormInputGroup" placeholder="Name">
+
+						
+							<label class="sr-only" for="inlineFormInputGroup">UCODE</label>
+							<input type="text" class="form-control" id="inlineFormInputGroup" placeholder="UCODE">
+
+
+							<button type="submit" class="btn btn-primary">Search</button>
+							 </form>
+
+							<input type="text" id="myInput" onkeyup="myFunction()" placeholder="Search for names.." title="Type in a name">
+
+							<table class="table">
+								<thead>
+									<tr>
+										<th style="text-align:center">ID</th>
+										<th style="text-align:center">Status</th>
+										<th style="text-align:center">Name</th>
+										<th style="text-align:center">UCODE</th>
+										<th style="text-align:center">NYEAR</th>
+										<th style="text-align:center">Year</th>
+										<th style="text-align:center">TCODE</th>
+										<th style="text-align:center">Test Center</th>
+										<th style="text-align:center">ASSG</th>
+										<th style="text-align:center">Profile</th>
+									</tr>
+								<tbody>
+
+								<!--Table. Calls mysql to show the data-->
+								<?php
+								$servername = "localhost";  
+								$username = "root";
+								$password = "";
+								$dbname = "upcatdb";
+
+								// Create connection
+								$conn = new mysqli($servername, $username, $password, $dbname);
+								// Check connection
+								if ($conn->connect_error) {
+										die("Connection failed: " . $conn->connect_error);
+								} 
+
+								$sql = "SELECT * FROM `DIRINFO`  LIMIT 10, 20" ;
+								$result = $conn->query($sql);
+
+								if ($result->num_rows > 0) {
+										// output data of each row
+
+
+										while($row = $result->fetch_assoc()) {
+
+												echo '<tr>';
+												echo '<td align="center">' .$row["ID"] . '</td>'; 
+												echo '<td align="center">' .$row["STATUS"] . '</td>';
+												echo '<td align="center">' .$row["NAME"] . '</td>';
+												echo '<td align="center">' .$row["UCODE"] . '</td>';
+												echo '<td align="center">' .$row["NYR"] . '</td>';
+												echo '<td align="center">' .$row["YEAR"] . '</td>';
+												echo '<td align="center">' .$row["TCTRCODE"] . '</td>';
+												echo '<td align="center">' .$row["testcenter"] . '</td>';
+												echo '<td align="center">' .$row["ASSG"] . '</td>';
+												echo '<td align="center"><a href="viewprofile.php?id='.$row["ID"].'">View Profile</a></td>';
+												// '<div class="dropdown">
+												//   <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">View Profile
+												//   <span class="caret"></span></button>
+												//   <ul class="dropdown-menu">
+												//     <li>photo</li>' .
+												//     '<li>' .$row["COL 1"] . '</li>' .
+												//     '<li>' .$row["COL 2"]. '</li>' .
+												//     '<li>' .$row["COL 3"]. '</li>' .
+												//     '<li><a href="#">What do we place here again?</a></li>
+
+												//   </ul>
+												// </div>' 										. '<td>';
+												echo '</tr>';
+										}
+								} else {
+										echo "0 results";
+								}
+
+
+								
+								?>
+								<!--Filter-->
+								<script>
+									function myFunction() {
+										var input, filter, table, tr, td, i;
+										input = document.getElementById("myInput");
+										filter = input.value.toUpperCase();
+										table = document.getElementById("myTable");
+										tr = table.getElementsByTagName("tr");
+
+										for (i = 0; i < tr.length; i++) {
+											td = tr[i].getElementsByTagName("td")[0];
+											if (td) {
+												if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+													tr[i].style.display = "";
+												} else {
+													tr[i].style.display = "none";
+												}
+											}       
+										}
+									}
+								</script>
+								<script type='text/javascript'>
+								$('#button1').click(function(){
+								 $.ajax({
+								 type: "POST",
+								 url: "abc.php",
+								 data: "",
+								 success: function(msg){
+								     alert(msg);
+								 },
+								 error: function(XMLHttpRequest, textStatus, errorThrown) {
+								    alert("Some error occured");
+								 }
+								 });
+								});
+								</script>
+								</tbody>
+							</table>
+			</div>
+		</div>
+</div>
+</body>
+<?php
+$myfile = fopen("newfile.txt", "w") or die("Unable to open file!");
+
+$txt = "Jane Doe\n";
+fwrite($myfile, $txt);
+
+
+if ($result->num_rows > 0) {
+		// output data of each row
+
+
+		while($row = $result->fetch_assoc()) {
+			$txt = "Jane Doe\n";
+		fwrite($myfile, $txt);
+				echo '<tr>';
+				echo '<td align="center">' .$row["ID"] . '</td>'; 
+				echo '<td align="center">' .$row["STATUS"] . '</td>';
+				echo '<td align="center">' .$row["NAME"] . '</td>';
+				echo '<td align="center">' .$row["UCODE"] . '</td>';
+				echo '<td align="center">' .$row["NYR"] . '</td>';
+				echo '<td align="center">' .$row["YEAR"] . '</td>';
+				echo '<td align="center">' .$row["TCTRCODE"] . '</td>';
+				echo '<td align="center">' .$row["testcenter"] . '</td>';
+				echo '<td align="center">' .$row["ASSG"] . '</td>';
+				echo '<td align="center"><a href="viewprofile.php?id='.$row["ID"].'">View Profile</a></td>';
+							
+				echo '</tr>';
+		}
+}
+
+
+$conn->close();
+
+
+fclose($myfile);
+?>
+</html>
