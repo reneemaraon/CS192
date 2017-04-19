27d823c2d4e64a0b99359f3677250e5e998a3287
LeeSicat <leesicat1@gmail.com>
2017-04-19 23:24:47 +0800

    Add files via upload

---
+++ Ucode/decrypter.py
@@ -0,0 +1,48 @@
+import random 
+code = "hwxhseahbeotreotwkoik"
+code2 = []
+
+print "abehkorstwx hwxhseahbeotreotwkoik"
+z = "ethe foreconsion standa"
+a = "a"
+b = "b"
+e = "t"
+h = "e"
+k = 'k'
+o = "o"
+r = 'r'
+s = 's'
+t = 't'
+w = 'w'
+x = "x"
+foo2 = ["a","b","c",'d',"e","f","g","h","i","j",'k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+
+
+
+for x in code:
+	if x == "a":
+		code2.append(a)
+	if x == "b":
+		code2.append(b)
+	if x == "e":
+		code2.append(e)
+	if x == "h":
+		code2.append(h)
+	if x == "k":
+		code2.append(k)
+	if x == "o":
+		code2.append(o)
+	if x == "r":
+		code2.append(r)
+	if x == "s":
+		code2.append(s)
+	if x == "t":
+		code2.append(t)
+	if x == "w":
+		code2.append(w)
+	if x == "x":
+		code2.append(x)
+code3 =""
+for x in code2:
+	code3+=x
+print code3
