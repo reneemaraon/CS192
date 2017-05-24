f1d5b934b4e0ca31b6aed39a06466f6491e715ac
LeeSicat <leesicat1@gmail.com>
2017-05-25 02:13:16 +0800

    Add files via upload

---
+++ selectall.php
@@ -0,0 +1,21 @@
+<?php
+								$servername = "localhost";  
+								$username = "root";
+								$password = "";
+								$dbname = "upcatdb";
+
+								// Create connection
+								$conn = new mysqli($servername, $username, $password, $dbname);
+								// Check connection
+								if ($conn->connect_error) {
+										die("Connection failed: " . $conn->connect_error);
+								} 
+
+								$sql = "UPDATE Invites SET invite = 1;";
+								$result = $conn->query($sql);
+
+
+								$conn->close();
+
+								?>
+<script type="text/javascript">location.href = 'Invitation';</script>
